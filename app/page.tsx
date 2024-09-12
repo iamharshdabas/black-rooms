722638bae0f0e6ffbcb5eadd04902aff1dc19b8f
Harsh Dabas <iamharshdabas@gmail.com>
2024-09-12 23:07:26 +0530

    feat(home): add hero section and product showcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
+import { Button } from "@nextui-org/button"
+
+import ProductShowcase from "./_components/product-showcase"
+
 import { bg, subtitle, title } from "@/config/primitives"
 
 export default function Home() {
   return (
-    <section>
+    <section className="flex flex-col items-center gap-16">
       <div className={bg({ className: "text-center" })}>
-        <h1 className={title()}>Make beautiful websites</h1>
+        <h1 className={title()}>Empower Your Connections with Black Rooms</h1>
         <br />
-        <h1 className={title()}>regardless of your design experience.</h1>
+        <h1 className={title()}>Where Innovation Meets Engagement.</h1>
         <h2 className={subtitle({ class: "pt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Transform your interaction landscape with custom-built communities designed to inspire and
+          connect.
         </h2>
       </div>
+
+      <div className="max-w-xs">
+        <div className="flex w-full flex-col gap-4">
+          <Button fullWidth className="bg-foreground font-bold text-background" variant="shadow">
+            Start Building Your Community
+          </Button>
+          <Button fullWidth variant="ghost">
+            Watch Our Demo
+          </Button>
+        </div>
+      </div>
+
+      <ProductShowcase />
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+      },
+    },
+  },
   darkMode: "class",
   plugins: [nextui()],
 }
