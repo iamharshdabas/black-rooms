65b22816ba936a3e695b3c31deb4e00a80fe5d9c
Harsh Dabas <iamharshdabas@gmail.com>
2024-09-18 23:22:49 +0530

    chore(db): update users schema

--- server/action/user/create.ts
+++ server/action/user/create.ts
@@ -4,5 +4,5 @@ import { db } from "@/server/db"
 import { users } from "@/server/schema"
 
 export default async function createUserAction(userId: string) {
-  await db.insert(users).values({ user_id: userId }).execute()
+  await db.insert(users).values({ clerk_id: userId }).execute()
 }
--- server/schema.ts
+++ server/schema.ts
@@ -1,6 +1,11 @@
+import { createId } from "@paralleldrive/cuid2"
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
-  user_id: text("user_id").primaryKey().notNull(),
+  user_id: text("user_id")
+    .primaryKey()
+    .notNull()
+    .$defaultFn(() => createId()),
   createTs: timestamp("create_ts").defaultNow().notNull(),
+  clerk_id: text("clerk_id").notNull(),
 })
